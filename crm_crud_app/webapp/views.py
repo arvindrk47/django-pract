fc871f0e72c4b0aeef5923b233759ac6177415a2
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2023-09-25 21:22:19 +0530

    updated

--- crm_crud_app/webapp/views.py
+++ crm_crud_app/webapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'webapp/index.html')
+
+
+# Register a user
+
+def register(request):
+    form  = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            
+    context = {'form':form}
+    return render(request, 'webapp/register.html', context=context)
